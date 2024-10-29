9da0db5f9f6316d692d3775ca22a54480635d3ac
IsaacMPereira <isaacp.menezesp@gmail.com>
2024-10-28 22:00:46 -0300

    Commit com código de tamanho de string rodando

--- avulsos/tamString.c
+++ avulsos/tamString.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 int main(){
-	long int count;
-	char aux[] = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed est eros, vulputate eget sodales nec, malesuada vel ante. Quisque volutpat risus in dolor sollicitudin a commodo libero suscipit. Nunc ultrices risus ut arcu hendrerit aliquet. Integer sit amet eros eu augue porttitor dictum. Quisque vel ante est, at lacinia tortor. Curabitur lacus purus, malesuada sit amet dapibus ac, placerat id leo. Nullam vel ultrices augue. Donec posuere convallis purus, sit amet imperdiet leo laoreet at. Quisque aliquam, urna non interdum lobortis, leo diam tincidunt lectus, non ultricies quam purus eget quam.";
-	
-	for(int i=0; i<1000; i++){
+	int count = 0;
+	char aux[1000];	
+
+	printf("Digite: ");
+	fgets(aux, sizeof(aux), stdin);
+	while(aux[count] != '\0'){
 		count++;
-		if(aux[i] == '\0'){
-			break;
-		}
 	}
-	printf("Count: %ld\n", count);
+
+	printf("Count: %d\n", count-1);
 
 	return 0;
 }
