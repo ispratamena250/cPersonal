6baef632c1add5a1ab378be5ef7313bf030c2664
IsaacMPereira <isaacp.menezesp@gmail.com>
2024-11-19 12:48:27 -0300

    Commit com lógica da string invertida funcionando

--- avulsos/stringInvertida.c
+++ avulsos/stringInvertida.c
@@ -6,8 +6,11 @@ int main(){
 	char c[100];
 
 	fgets(c, sizeof(c), stdin);
-	for(int i=0, j=strlen(c); i<strlen(c), j<0; i++, j--){
-			printf("%c ", c[j]);
+	int tam = strlen(c);
+
+	for(int i=0; i<strlen(c); i++){
+		printf("%c ", c[tam-1]);
+		tam--;
 	}
 	printf("\n");
 
