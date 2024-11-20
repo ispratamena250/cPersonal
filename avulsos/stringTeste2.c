0709f6ba43f5d777f9aaa0193e6c16794463f0fc
IsaacMPereira <isaacp.menezesp@gmail.com>
2024-11-19 23:37:53 -0300

    Commit com alterações e exclusões

--- avulsos/stringTeste2.c
+++ avulsos/stringTeste2.c
@@ -2,13 +2,21 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define TAM 100
+
 int main(){
-	char s[100];
+	char s[TAM], c2[TAM];
 
 	fgets(s, sizeof(s), stdin);
+	fgets(c2, sizeof(c2), stdin);
 
 	for(int i=0; i<strlen(s); i++){
 		printf("%c ", s[i]);
+	}
+	printf("\n");
+
+	for(int i=0; i<strlen(c2); i++){
+		printf("%c ", c2[i]);
 	}
 	printf("\n");
 
