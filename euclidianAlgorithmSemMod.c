bca472a34981f9be161564909c33af8e982c580b
IsaacMPereira <isaacp.menezesp@gmail.com>
2024-12-06 15:49:04 -0300

    Commit com adição de contador

--- euclidianAlgorithmSemMod.c
+++ euclidianAlgorithmSemMod.c
@@ -5,7 +5,7 @@
 #include <stdio.h>
 
 int main(){
-	int a, b, c=0, aux1, auxA, auxB;
+	int a, b, c=0, aux1, auxA, auxB, count=0;
 
 	printf("Type two natural numbers: ");
 	scanf("%d %d", &a, &b);
@@ -19,16 +19,18 @@ int main(){
 	}
 	do{
 		c = a - b;
+		count++;
 		printf("%d = %d - %d\n", c, a, b);
 		a = c;
 		if(c < b){
 			a = b;
 			b = c;
 			c = a - b;
+			count++;
 			printf("%d = %d - %d\n", c, a, b);
 		}
 	}while(c != b && c != 1);
-	printf("gcd(%d, %d) = %d\n", auxA, auxB, c);
+	printf("gcd(%d, %d) = %d\nOperacoes: %d\n", auxA, auxB, c, count);
 	
 	return 0;
 }
