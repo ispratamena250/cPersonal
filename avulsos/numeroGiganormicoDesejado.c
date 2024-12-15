8f9ceda5ee22b662403fc25004039d36f95931c6
IsaacMPereira <isaacp.menezesp@gmail.com>
2024-12-15 17:42:41 -0300

    Commit com melhoras e referências no código!

--- avulsos/numeroGiganormicoDesejado.c
+++ avulsos/numeroGiganormicoDesejado.c
@@ -1,19 +1,21 @@
 /*
  * C programm to generate a giant number (string) with a desired size
- * Run like this: $numeroGiganormicoDesejado n
- * If you want to copy the output to clipboard: ./numeroGiganormicoDesejado n | xclip -selection clipboard
+ * Compile like this: $gcc -Wall numeroGiganormicoDesejado.c -lm -o numeroGiganormicoDesejado.x 
+ * If you want to copy the output to clipboard: ./numeroGiganormicoDesejado.x n | xclip -selection clipboard
  * To install xclip: sudo apt-get install xclip
+ * Explanetion about line 37 formula: POMMERSHEIM, James. Number Theory. EUA: Jhon Wiley & Sons, Inc, 2010. p. 400
  * */
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 
 int main(int argc, char *argv[]){
 	int n, aux;
 	srand(time(NULL));
 
 	if(argc != 2){
-		printf("Erro! Run like this: $ ./numeroGiganormicoDesejado n\n");
+		printf("Erro! Run like this: $ ./numeroGiganormicoDesejado.x n\n");
 		exit(1);
 	}
 	n = atoi(argv[1]);
@@ -32,7 +34,7 @@ int main(int argc, char *argv[]){
 	for(int i=0; i<n; i++){
 		printf("%d", s[i]);
 	}
-	printf("\n");
+	printf("\nY = %f\n", 13.24 * pow(n, 0.333333333) + 1928.6);
 
 	return 0;
 }
