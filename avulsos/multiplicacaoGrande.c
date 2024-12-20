547e12c23c83cca4797451c242856830eefa1ab2
IsaacMPereira <isaacp.menezesp@gmail.com>
2024-12-20 19:56:18 -0300

    Commit com pensamentos e lembranças

---
+++ avulsos/multiplicacaoGrande.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+	long long n = 67108864;
+	printf("%lld\n", n*2);
+
+	return 0;
+}
---
+++ avulsos/relembrandoDefine.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+#define TAM 3
+
+int main(){
+	printf("%d\n", TAM+2);
+
+	return 0;
+}
