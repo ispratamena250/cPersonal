02170bd30521f6d74374932eb4594607ec6d4ec7
IsaacMPereira <isaacp.menezesp@gmail.com>
2025-01-02 15:52:25 -0300

    Esse código tá quebrado

--- gslProgramms/multMatrix1.c
+++ gslProgramms/multMatrix1.c
@@ -7,21 +7,21 @@
 #include <stdio.h>
 #include <gsl/gsl_matrix.h>
 
-int main() {
+int main() { //Esse código tá quebrado.
     // Cria duas matrizes 2x2
     gsl_matrix *a = gsl_matrix_alloc(2, 2);
     gsl_matrix *b = gsl_matrix_alloc(2, 2);
 
     // Preenche as matrizes com valores
-    gsl_matrix_set(a, 0, 0, 0.0);
-    gsl_matrix_set(a, 0, 1, -1.0);
-    gsl_matrix_set(a, 1, 0, 1.0);
-    gsl_matrix_set(a, 1, 1, 0.0);
+    gsl_matrix_set(a, 0, 0, 0);
+    gsl_matrix_set(a, 0, 1, -1);
+    gsl_matrix_set(a, 1, 0, 1);
+    gsl_matrix_set(a, 1, 1, 1);
 
-    gsl_matrix_set(b, 0, 0, -1.0);
-    gsl_matrix_set(b, 0, 1, 0.0);
-    gsl_matrix_set(b, 1, 0, 0.0);
-    gsl_matrix_set(b, 1, 1, -1.0);
+    gsl_matrix_set(b, 0, 0, 0);
+    gsl_matrix_set(b, 0, 1, -1);
+    gsl_matrix_set(b, 1, 0, 1);
+    gsl_matrix_set(b, 1, 1, 1);
 
 	for(size_t i=0; i<2; i++){
 		for(size_t j=0; j<2; j++){
