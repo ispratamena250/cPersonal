da815320c6ca29869a2fd790887d9cbf81fa96b7
IsaacMPereira <isaacp.menezesp@gmail.com>
2025-05-21 13:44:26 -0300

    Alterações de sintaxe e uso

--- avulsos/tabelaVerdade.c
+++ avulsos/tabelaVerdade.c
@@ -15,55 +15,55 @@
 int lastLine_numOfBits();
 
 int main(int argc, char *argv[2]){ //Para valores muito altos, como 45, faltam ajustes
-  if(argc != 2){
-    printf("Erro! Run: $ ./tabelaVerdade.x var\n");
-    exit(1);
-  }
+	if(argc != 2){
+		printf("Erro! Run: $ ./tabelaVerdade.x var\n");
+		exit(1);
+	}
 
-  char var = atoi(argv[1]);
-  mpz_t n;
-  mpz_init(n);
+	char var = atoi(argv[1]);
+	mpz_t n;
+	mpz_init(n);
 
-  int aux_lastLine = atoi(argv[1]);
-  int last = lastLine_numOfBits(aux_lastLine);
+	int aux_lastLine = atoi(argv[1]);
+	int last = lastLine_numOfBits(aux_lastLine);
 
-  for(int i=0; i<(int)pow(2, var); i++){
-    char aux[100];
-    sprintf(aux, "%d", i);
-    mpz_set_str(n, aux, 10);
-    char *bin = mpz_get_str(NULL, 2, n);
-    int aux_sprintf = atoi(bin);
+	for(int i=0; i<(int)pow(2, var); i++){
+		char aux[100];
+		sprintf(aux, "%d", i);
+		mpz_set_str(n, aux, 10);
+		char *bin = mpz_get_str(NULL, 2, n);
+		int aux_sprintf = atoi(bin);
 
-    char c[100];
-    sprintf(c, "%0*d", last, aux_sprintf);
-    printf("%do -> %s\n", i, c);
+		char c[100];
+		sprintf(c, "%0*d", last, aux_sprintf);
+		printf("%do -> %s\n", i, c);
 
-    free(bin);
-  }
-  
-  mpz_clear(n);
+		free(bin);
+	}
 
-  printf("\n");
-  return 0;
+	mpz_clear(n);
+
+	printf("\n");
+	return 0;
 }
 
 /*
  *  A function that find the number of bits of the last line of the truth table
  * */
 int lastLine_numOfBits(int z){ 
-    int aux = (int)pow(2, z-1);
-  char s[100];
-  sprintf(s, "%d", aux);
+	int aux = (int)pow(2, z-1);
+	char s[100];
+	sprintf(s, "%d", aux);
 
-  mpz_t n;
-  mpz_init(n);
+	mpz_t n;
+	mpz_init(n);
 
-  mpz_set_str(n, s, 10);
-  char *bin = mpz_get_str(NULL, 2, n);
-  int size = strlen(bin);
+	mpz_set_str(n, s, 10);
+	char *bin = mpz_get_str(NULL, 2, n);
+	int size = strlen(bin);
 
-  mpz_clear(n);
-  free(bin);
+	mpz_clear(n);
+	free(bin);
 
-  return size;
+	return size;
 }
