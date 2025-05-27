0c58223e28e1eeba3c90850c8541e125d68270a0
IsaacMPereira <isaacp.menezesp@gmail.com>
2025-05-27 15:14:37 -0300

    Estudo de lista simples e pilha

---
+++ eda1/p4.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct pilha Pilha;
+typedef struct no No;
+
+struct pilha{
+	No* topo;
+};
+
+struct no{
+	No* prox;
+	int item;
+};
+
+Pilha* cria(){
+	Pilha* p = (Pilha*) malloc(sizeof(Pilha));
+	p->topo = NULL;
+
+	return p;
+}
+
+void insere(Pilha* p, int z){
+	No* novo = (No*) malloc(sizeof(No));
+	novo->item = z;
+	novo->prox = NULL;
+
+	novo->prox = p->topo;
+	p->topo = novo;
+}
+
+int remover(Pilha* p, int z){
+	if(p->topo == NULL){
+		return 1;		
+	}else{
+		No* atual = p->topo;
+		p->topo = atual->prox;
+		free(atual);
+		return 0;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
