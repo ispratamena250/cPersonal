6d3093fbf81ac7a2cb2775459795af56746da633
IsaacMPereira <isaacp.menezesp@gmail.com>
2025-06-19 21:33:40 -0300

    Ponteiros em loop

---
+++ avulsos/ponteiros/loopAddress.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+	int v[10];
+
+	for(int i=0; i<10; i++){
+		v[i] = i;
+	}
+
+	for(int i=0; i<10; i++){
+		printf("%p\n", &v[i]);
+	}
+
+	printf("\n");
+	return 0;
+}
